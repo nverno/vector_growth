1b447b35a605890a3da60da9dec478464a7a7955
Noah Peart <noah.v.peart@gmail.com>
2015-08-19 17:13:57 -0400

    Batch update

--- R/globals.R
+++ R/globals.R
@@ -3,13 +3,13 @@
 ## Description: 
 ## Author: Noah Peart
 ## Created: Thu Aug 13 19:57:04 2015 (-0400)
-## Last-Updated: Fri Aug 14 13:04:54 2015 (-0400)
+## Last-Updated: Wed Aug 19 16:42:28 2015 (-0400)
 ##           By: Noah Peart
 ######################################################################
 
 dataloc <- c("../../treedata/")           # locations to look for data
 datafiles <- c("pp.csv", "transect.csv")  # data files
-temploc <- "../temp/"                      # where to store temporary data
+temploc <- "../temp"                      # where to store temporary data
 
 ################################################################################
 ##
@@ -20,3 +20,11 @@ require(shiny)
 require(plyr)
 require(dplyr)
 require(ggplot2)
+require(grid)  # arrows
+
+################################################################################
+##
+##                              Plotting stuff
+##
+################################################################################
+defaults <- theme_bw()
---
+++ R/growth_vectors.R
@@ -0,0 +1,61 @@
+### growth_vectors.R --- 
+## Filename: growth_vectors.R
+## Description: 
+## Author: Noah Peart
+## Created: Wed Aug 19 14:42:19 2015 (-0400)
+## Last-Updated: Wed Aug 19 17:05:50 2015 (-0400)
+##           By: Noah Peart
+######################################################################
+
+vgInds <- reactive({
+    with(pp, PPLOT %in% input$vgPlot & !is.na(pp[,input$vgX]) & !is.na(pp[,input$vgY]))
+})
+
+vecGrowth <- renderPlot({
+    p1 <- ggplot(pp[vgInds(),], aes_string(input$vgX, input$vgY, color="YEAR")) +
+      geom_path(alpha=0.5, aes(group=id), arrow=arrow(angle=20, length=unit(0.4, "cm"), type="closed"),
+                na.rm=T) +
+      defaults
+    
+    if (input$vgPoints)
+        p1 <- p1 + geom_point(alpha=0.5)
+    
+    if (input$vgSplit)
+        if (input$vgWrap) p1 <- p1 + facet_wrap(~ SPEC)
+        else p1 <- p1 + facet_grid(~ SPEC)
+    p1
+})
+
+vecUI <- renderUI({
+    
+    sidebarLayout(
+        sidebarPanel(
+            selectInput("vgPlot", "Choose Plot:", choices=unique(pp$PPLOT)),
+            selectInput("vgY", "Y", choices=c("HT", "HTOBS", "DBH", "BV", "BA")),
+            selectInput("vgX", "X", choices=c("DBH", "BV", "BA", "HT", "HTOBS")),
+            checkboxInput("vgSplit", "Split by Species"),
+            conditionalPanel(
+                condition = "input.vgSplit == true",
+                checkboxInput("vgWrap", "Wrap Output")
+            ),
+            checkboxInput("vgPoints", "Points"),
+            conditionalPanel(
+                condition = "input.vgY == 'HT' && input.vgX == 'DBH'",
+                checkboxInput("vgShowFit", "Show fitted")
+            ),
+            checkboxInput("vgSmooth", "Add smoothed spline"),
+            conditionalPanel(
+                condition = "input.vgSmooth == true",
+                selectInput("vgSmoothMethod", "Method", list("loess", "lm"))
+            ),
+
+            hr(),
+            helpText("Aesthetics", style="color: #48ca3b;"),
+            checkboxInput("vgArrowEnds", "Open Arrows")
+        ),
+
+        mainPanel(
+            vecGrowth
+        )
+    )
+})
